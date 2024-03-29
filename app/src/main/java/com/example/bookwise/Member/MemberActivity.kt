2c0e68abb6ce08991590194d432a7df16c281da9
Tishan <tishan.koleh@trustt.com>
2024-03-29 13:12:48 +0530

    Member Dashboard Initiated

--- app/src/main/java/com/example/bookwise/Member/MemberActivity.kt
+++ app/src/main/java/com/example/bookwise/Member/MemberActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.bookwise.R
+import com.example.bookwise.databinding.ActivityMemberBinding
 
 class MemberActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMemberBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_member)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_member)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.toolbar.setNavigationOnClickListener {
+            binding.main.open()
         }
     }
 }
