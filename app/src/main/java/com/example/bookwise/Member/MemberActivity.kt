855b451d5d046e6aef65e2b44a7fbdaf526b467e
Tishan <tishan.koleh@trustt.com>
2024-03-29 19:42:57 +0530

    Back Action Fixed

--- app/src/main/java/com/example/bookwise/Member/MemberActivity.kt
+++ app/src/main/java/com/example/bookwise/Member/MemberActivity.kt
@@ -1,7 +1,11 @@
 package com.example.bookwise.Member
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
@@ -13,6 +17,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.example.bookwise.MainActivity
 import com.example.bookwise.R
 import com.example.bookwise.databinding.ActivityMemberBinding
 import com.google.android.material.navigation.NavigationView
@@ -20,7 +25,6 @@ import com.google.android.material.navigation.NavigationView
 class MemberActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMemberBinding
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,52 +35,77 @@ class MemberActivity : AppCompatActivity() {
             insets
         }
 
-        //Click menu button to open Navigation DRawer
+        //Click menu button to open Navigation Drawer
         binding.toolbar.setNavigationOnClickListener {
             binding.main.open()
         }
 
 
+        drawerLayout = binding.main
 
+        //The Default Screen Is home Screen So selecting option Home By default
+        val menu = binding.navigationView.menu
+        val menuItem = menu.findItem(R.id.homeOption)
+        menuItem.isChecked = true
 
-
-       // val navController = findNavController(R.id.navigation_view)
-
-
-       // State selector for Items
+        // Select Items To perform Desired Actions
         binding.navigationView.setNavigationItemSelectedListener {
             it.isChecked = true
+            binding.main.close()
             when(it.itemId){
-
                 R.id.homeOption->{
                     findNavController(R.id.fragmentContainerView_member).navigate(R.id.homeFragment)
                 }
-
                 R.id.books ->{
                     findNavController(R.id.fragmentContainerView_member).navigate(R.id.booksFragment)
-
                 }
-
                 R.id.return_books ->{
                     findNavController(R.id.fragmentContainerView_member).navigate(R.id.returnFragment)
                 }
-
                 R.id.fine ->{
                     findNavController(R.id.fragmentContainerView_member).navigate(R.id.fineFragment)
                 }
-
-
+                R.id.logout->{
+                    finish()
+                }
             }
-
             return@setNavigationItemSelectedListener true
-
-
-            }
+        }
     }
 
-//    fun navigateFragments(fragment1 : String, fragment2: String){
-//        var id: String
-//
-//    }
+    override fun onBackPressed() {
 
+        //Close the drawer if open else navigate back to home else perform default back Action
+
+        val home = findNavController(R.id.fragmentContainerView_member).currentDestination?.id
+        if(binding.main.isDrawerOpen(GravityCompat.START)){
+            binding.main.close()
+        } else if( home != R.id.homeFragment){
+            findNavController(R.id.fragmentContainerView_member).navigate(R.id.homeFragment)
+            val menu = binding.navigationView.menu
+            val menuItem = menu.findItem(R.id.homeOption)
+            menuItem.isChecked = true
+        } else if(home == R.id.homeFragment){
+            showExitAlertDialog()
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+
+    fun showExitAlertDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Exit")
+        builder.setMessage("Are you sure you want to exit the app?")
+        builder.setPositiveButton("Yes") { dialogInterface: DialogInterface, i: Int ->
+            // Exit the app
+            finishAffinity()
+        }
+        builder.setNegativeButton("No") { dialogInterface: DialogInterface, i: Int ->
+            // Dismiss the dialog
+            dialogInterface.dismiss()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
+    }
 }
