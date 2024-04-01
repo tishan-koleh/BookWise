028559f31598f60da0aca1a422b7ae7a52c0c32b
Tishan <tishan.koleh@trustt.com>
2024-04-01 12:04:08 +0530

    Welcome Screen

--- app/src/main/java/com/example/bookwise/Member/MemberActivity1.kt
+++ app/src/main/java/com/example/bookwise/Member/MemberActivity1.kt
@@ -1,4 +1,4 @@
-package com.example.bookwise
+package com.example.bookwise.Member
 
 import android.content.Intent
 import android.os.Bundle
@@ -6,20 +6,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.bookwise.Member.MemberActivity
-import com.example.bookwise.Member.MemberActivity1
+import androidx.databinding.DataBindingUtil
+import com.example.bookwise.R
+import com.example.bookwise.databinding.ActivityMember1Binding
+import com.example.bookwise.databinding.ActivityMemberBinding
 
-class MainActivity : AppCompatActivity() {
+class MemberActivity1 : AppCompatActivity() {
+    private lateinit var binding:ActivityMember1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_member1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val intent = Intent(this, MemberActivity1::class.java)
-        startActivity(intent)
+
+
+        binding.button3.setOnClickListener {
+            startActivity(Intent(this,MemberActivity::class.java))
+        }
+
     }
 }
