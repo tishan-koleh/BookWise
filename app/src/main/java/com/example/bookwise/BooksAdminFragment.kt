2aefa02d4abde8248d41cc65c35da0a4f4b9fc19
Tishan <tishan.koleh@trustt.com>
2024-04-12 17:24:21 +0530

    Admin Books View

--- app/src/main/java/com/example/bookwise/BooksAdminFragment.kt
+++ app/src/main/java/com/example/bookwise/BooksAdminFragment.kt
@@ -2,14 +2,26 @@ package com.example.bookwise
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookwise.Admin.AdminAddBook
+import com.example.bookwise.RecyclerView.ProgrammingAdapter
+import com.example.bookwise.RecyclerView.ProgrammingAdapterAdminBooks
+import com.example.bookwise.Retrofit.ApiService
+import com.example.bookwise.Retrofit.RetrofitHepler
+import com.example.bookwise.ViewModels.MainVIewModelFactory
+import com.example.bookwise.ViewModels.MainViewModel
 import com.example.bookwise.databinding.FragmentBooksAdminBinding
 import com.google.android.material.navigation.NavigationView
+import retrofit2.Retrofit
+import retrofit2.create
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,10 +34,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BooksAdminFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentBooksAdminBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    lateinit var binding : FragmentBooksAdminBinding
 
 
 
@@ -49,12 +62,49 @@ class BooksAdminFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_books_admin, container, false)
+        val apiService = RetrofitHepler.getInstance().create(ApiService::class.java)
+        val factory = MainVIewModelFactory(apiService)
+        viewModel = ViewModelProvider(this,factory)[MainViewModel::class.java]
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.addBookButton.setOnClickListener {
             startActivity(Intent(this@BooksAdminFragment.context,AdminAddBook::class.java))
         }
+        val recyclerView = binding.recyclerViewAdminBooks
+        val adapter = ProgrammingAdapterAdminBooks(viewModel)
+        adapter.submitList(listOf())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        viewModel.getBookList()
+        viewModel.book.observe(viewLifecycleOwner, Observer {
 
-        return binding.root
+            adapter.submitList(it)
+        })
+
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                showProgressBar()
+                binding.recyclerViewAdminBooks.visibility = View.GONE
+            } else {
+                Handler().postDelayed({
+                    hideProgressBar()
+                    binding.recyclerViewAdminBooks.visibility = View.VISIBLE
+                },1000)
+
+            }
+        })
+
+    }
+
+    private fun showProgressBar() {
+        binding.progressBarAdminBook.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBarAdminBook.visibility = View.GONE
     }
 
 
--- app/src/main/java/com/example/bookwise/RecyclerView/ProgrammingAdapterAdminBooks.kt
+++ app/src/main/java/com/example/bookwise/RecyclerView/ProgrammingAdapterAdminBooks.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -18,7 +19,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class ProgrammingAdapter(private val mainViewModel: MainViewModel) : ListAdapter<BookListItem, ProgrammingAdapter.ProgrammingViewHolder>(diffUtil()) {
+class ProgrammingAdapterAdminBooks(private val mainViewModel: MainViewModel) : ListAdapter<BookListItem, ProgrammingAdapterAdminBooks.ProgrammingViewHolder>(diffUtil()) {
 
     class ProgrammingViewHolder(view: View,mainViewModel: MainViewModel) : RecyclerView.ViewHolder(view) {
 
@@ -26,9 +27,7 @@ class ProgrammingAdapter(private val mainViewModel: MainViewModel) : ListAdapter
         private val authorName: TextView = view.findViewById(R.id.author_name)
         private val categoryName: TextView = view.findViewById(R.id.category_name)
         private val quantity: TextView = view.findViewById(R.id.quantity)
-        private val button :Button = view.findViewById(R.id.add_quantity_button)
-
-
+        private val button :ImageView = view.findViewById(R.id.add_button)
 
         fun bind(item: BookListItem,mainViewModel: MainViewModel) {
             CoroutineScope(Dispatchers.Main).launch {
@@ -36,23 +35,16 @@ class ProgrammingAdapter(private val mainViewModel: MainViewModel) : ListAdapter
                 authorName.text = item.author.name
                 categoryName.text = item.genre.name
                 quantity.text = item.quantity.toString()
-                if (item.quantity==0){
-                    button.isEnabled = false
-                }
+
             }
             button.setOnClickListener {
-                Log.i("BUTTON ACTION","Book Id-${item.id}")
-                mainViewModel.initiateTransaction(SharedPreferencesHelper.readInt(Utils.card_id,-1),item.id)
 
             }
         }
-
-
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgrammingViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_list_items, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_list_item_admin, parent, false)
         return ProgrammingViewHolder(view,mainViewModel)
     }
 
